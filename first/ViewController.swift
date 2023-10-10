78689577a8e457881cd136367423714e49b86ea4
N0ku <naku.pocket@gmail.com>
2023-10-10 14:59:58 +0200

    🌍 - add start project files

--- first/ViewController.swift
+++ first/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var videoBackground: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadVideoBackground()
     }
 
-
+    private func loadVideoBackground(){
+        let path = URL(fileURLWithPath: Bundle.main.path(forResource: "sky", ofType: "mp4")!)
+        
+        let player = AVPlayer(url: path)
+        
+        let layer = AVPlayerLayer(player: player)
+        layer.frame = self.videoBackground.frame
+        self.videoBackground.layer.addSublayer(layer)
+        layer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        
+        player.play()
+        
+        player.actionAtItemEnd = AVPlayer.ActionAtItemEnd.none
+        
+        NotificationCenter.default.addObserver(self,selector: #selector(ViewController.videoDidPlayToEnd(_:)),name:NSNotification.Name("AVPlayerItemDidPlayToEndTimeNotification"),object: player.currentItem)
+    }
+     
+    @objc func videoDidPlayToEnd(_ notification: Notification){
+        let player: AVPlayerItem = notification.object as! AVPlayerItem
+        player.seek(to: CMTime.zero)
+    }
 }
 
